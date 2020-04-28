4d50fdade2bd38f2a4f8a822c4f68af94ca1af54
Aaron Toth <atoth89@gmail.com>
2020-04-27 22:00:25 -0400

    Switch from pipenv to poetry

--- mygeotab/api.py
+++ mygeotab/api.py
@@ -513,7 +513,7 @@ class EntityList(UserList):
         except ImportError:
             raise ImportError("The 'pandas' package could not be imported")
         if normalize:
-            from pandas.io.json import json_normalize
+            from pandas import json_normalize
 
             return json_normalize(self.data)
         return pandas.DataFrame.from_dict(self.data)
